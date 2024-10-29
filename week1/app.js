75e5e42818b25ad007dde0501acdf42b9d10f0e9
Yipmong Said <Syipmong5@gmail.com>
2024-10-29 14:08:40 +0100

    Add missing configuration files and ignore unnecessary files

---
+++ week1/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { connectToDb, getDb } = require('./db');
+
+const app = express();
+let db;
+
+connectToDb((err) => {
+    if (!err) {
+        app.listen(3000, () => console.log('Listening on Port 3000'));
+        db = getDb();
+    }
+});
+
+app.get('/books', (_, res) => {
+    db.collection('books').find().sort({ author: 1 }).toArray()
+        .then(books => {
+            res.status(200).json(books);
+        })
+        .catch(err => {
+            res.status(500).json({ error: `could not fetch the documents ${err}` });
+        });
+});
